79905e338148010a0bb2b689e9495a515c606650
DeVeraGiovanni <bonhjodevera1414@gmail.com>
2024-02-03 22:21:06 +0800

    Changed the functionality base on image reference

--- resources/views/admin/feeding-program/feeding-program.blade.php
+++ resources/views/admin/feeding-program/feeding-program.blade.php
@@ -11,7 +11,46 @@
 @endsection
 
 {{-- CONTENT --}}
+
+
 @section('content')
+<style>
+
+#imageUpload {
+    
+}
+        .chip {
+            display: inline-block;
+            padding: 8px 12px;
+            border-radius: 20px;
+            font-size: 14px;
+        }
+
+        .chip-success {
+            background-color: green;
+            color: white;
+        }
+
+        .chip-start {
+            background-color: black;
+            color: white;
+        }
+
+        .chip-danger {
+            background-color: red;
+            color: white;
+        }
+
+        .chip-info {
+            background-color: blue;
+            color: white;
+        }
+
+        .chip-warning {
+            background-color: orange;
+            color: white;
+        }
+    </style>
     {{-- EDIT MODAL --}}
     <div id="editModal" class="modal" tabindex="-1" feeding_program="dialog">
         <div class="modal-dialog modal-lg modal-dialog-centered">
@@ -52,6 +91,18 @@
                             <div class="form-group col-md-12">
                                 <label class="required-input">Details</label>
                                 <textarea class="form-control" id="description_edit" name="description_edit" tabindex="2" rows="5"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-12">
+                                <label class="required-input">Progress</label>
+                                <select class="form-control" id="progress_edit" name="progress_edit">
+                                <option value="Coming Soon">Coming Soon</option>
+                                    <option value="Success">Success</option>
+                                    <option value="Postponed">Postponed</option>
+                                    <option value="Ongoing">Ongoing</option>
+                                    <option value="Cancelled">Cancelled</option>
+                                </select>
                             </div>
                         </div>
                         <div class="row">
@@ -143,27 +194,32 @@
             </div>
             <table class="table table-hover table-borderless" id="dataTable" style="width:100%">
                 <thead>
-                    <tr class="text-dark">
+                    <tr class="text-dark" >
                         <th class="not-export-column">ID</th>
                         <th class="not-export-column">Created at</th>
                         <th>Title</th>
+                        <th>Label</th>
                         <th>Date</th>
                         <th>Time</th>
                         <th>Status</th>
+                        <th >Progress</th>
+
                         <th class="not-export-column">Action</th>
                     </tr>
                 </thead>
                 <tbody>
 
                 </tbody>
-                <tfoot>
-                    <tr class="text-dark">
+                <tfoot >
+                    <tr class="text-dark" >
                         <th class="not-export-column">ID</th>
                         <th class="not-export-column">Created at</th>
                         <th>Title</th>
+                        <th>Label</th>
                         <th>Date</th>
                         <th>Time</th>
                         <th>Status</th>
+                        <th >Progress</th>
                     </tr>
                 </tfoot>
             </table>
@@ -176,33 +232,37 @@
             <div class="modal-content">
                 <div class="modal-header bg-primary text-white" style="border-radius: 0.5rem 0.5rem 0 0;">
                     <h5 class="modal-title" style="color: white;">Upload Images</h5>
-                </div>
+                </div >
                 <div class="modal-body">
-                    <form id="uploadForm">
-                        <div class="form-group">
-                            <label for="imageUpload" class="required-input">Select Images</label>
-                            <input type="file" class="form-control-file" id="imageUpload" name="images[]" multiple accept="image/*">
-                        </div>
-                    </form>
-                    <!-- Display selected images -->
+                    <!-- <div class="choose-file"> 
+                        <form id="uploadForm" >
+                            <div class="form-group" style="border-style:solid; border-width:thin; border-color:gray; border-radius:0.5rem">
+                                <label for="imageUpload" class="required-input" style="border-style:solid; border-width:thin; border-color:gray; padding:0.5rem;background-color:#5D88FFDC; color:white; border-radius:0.4rem">📂 Browse</label>
+                                <input type="file" class="form-control-file" id="imageUpload" name="images[]" multiple accept="image/*" style="display:none" >
+                            </div>
+                        </form>
+
+                    </div> -->
+
                     <div id="selectedImagesContainer" class="d-flex flex-wrap mb-3"></div>
-                    <!-- Image preview -->
                     <div id="imagePreviewContainer" class="d-flex flex-wrap"></div>
+                    <div class="button-container" style="display:flex; align-items:center; justify-content: center"> 
+                    <label class="btn btn-secondary">
+    Browse Images<input type="file" class="form-control-file" id="imageUpload" name="images[]" multiple accept="image/*" style="display:none" >
+</label>
+
+                    </div>
+                    
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" id="btnCloseModal">Close</button>
                     <button type="button" class="btn btn-primary" id="btnUploadFile">Upload</button>
                 </div>
             </div>
         </div>
     </div>
 
- 
-<!-- JATTT  -->
-<div id="imageDisplaySection">
-    <h5 class="card-title fw-semibold mb-4 text-black">Activity Images</h5>
-    <div id="activityImagesContainer" class="d-flex flex-wrap mb-3"></div>
-</div>
+
 </div>
 @endsection
 
@@ -210,6 +270,7 @@
 {{-- SCRIPTS --}}
 @section('scripts')
     <script>
+
         // START SCRIPT TAG
         $(document).ready(function() {
 
@@ -227,6 +288,9 @@
                     let title = $('#dataTable thead th').eq($(this).index()).text();
                     $(this).html('<input size="15" class="form-control" type="text" placeholder="' + title +
                         '" data-index="' + i + '" />');
+                columnDefs: [
+                    { targets: [7], className: 'text-center' } // Targets the "Progress" column (adjust index as needed)
+                ]
                 });
 
                 dataTable = $('#dataTable').DataTable({
@@ -254,6 +318,9 @@
                             data: "title",
                         },
                         {
+                            data: 'type'
+                        },
+                        {
                             data: "date_of_program",
                             render: function(data, type, row) {
                                 return moment(data).format('ll')
@@ -269,16 +336,44 @@
                             data: "status",
                         },
                         {
+                    data: 'progress', render: function(data) {
+                    let chipClass = '';
+
+                    switch (data) {
+                        case 'Coming Soon':
+                            chipClass = 'chip-start';
+                            break;
+                        case 'Success':
+                            chipClass = 'chip-success';
+                            break;
+                        case 'Postponed':
+                            chipClass = 'chip-warning';
+                            break;
+                        case 'On Going':
+                            chipClass = 'chip-info';
+                            break;
+                        case 'Cancelled':
+                            chipClass = 'chip-danger';
+                            break;
+                        default:
+                            chipClass = 'chip-info';
+                            break;
+                    }
+
+                    return `<div class="chip ${chipClass} btnEdit">${data}</div>`;
+                }
+                        },
+                        {
                             data: "deleted_at",
                             render: function(data, type, row) {
+                                // <button id="${row.id}" type="button" class="btn btn-sm btn-info btnView">View</button>
                                 console.log(data)
                                 if (data == null) {
                                     return `<div>
-                                        <button id="${row.id}" type="button" class="btn btn-sm btn-info btnView">View</button>
+                                    <button id="${row.id}" type="button" class="btn btn-sm btn-info btnUpload" data-id="${row.id}">Add pictures</button>                                        
                                         <button id="${row.id}" type="button" class="btn btn-sm btn-warning btnEdit">Edit</button>
                                         <button id="${row.id}" type="button" class="btn btn-sm btn-danger btnDelete">Delete</button>
-                                        <button id="${row.id}" type="button" class="btn btn-sm btn-info btnUpload" data-id="${row.id}">Upload</button>
-
+                                       
                                         </div>`;
                                 } else {
                                     return '<button class="btn btn-danger btn-sm">Activate</button>';
@@ -355,6 +450,8 @@
             }
             // REFRESH DATATABLE FUNCTION
 
+
+
             // CREATE FUNCTION
             $('#createForm').on('submit', function(e) {
                 e.preventDefault()
@@ -372,7 +469,9 @@
                 currDatetime = moment().format("YYYY-MM-DD HH:mm:ss");
                 form_data.date_posted = currDatetime
                 form_data.status = 'Draft';
-
+                form_data.progress = 'Coming Soon';
+                console.log("CREATE ACTIVITY FORM DATA")
+                console.log(form_data)
                 // ajax opening tag
                 $.ajax({
                     url: form_url,
@@ -437,7 +536,7 @@
                         $('#time_of_program_edit').val(data.time_of_program)
                         $('#description_edit').val(data.description)
                         $('#location_edit').val(data.location)
-
+                        $('#type_edit').val(data.type)
                         $('#editModal').modal('show');
                     },
                     error: function(error) {
@@ -581,7 +680,7 @@
 
 
 
-async function convertImageToFormDataAndInsert(file, uploadButtonId) {
+async function convertImageToFormDataAndInsert(file, uploadButtonId, referenceNumber) {
     return new Promise((resolve, reject) => {
         // Convert the image to base64
         const reader = new FileReader();
@@ -590,6 +689,7 @@ async function convertImageToFormDataAndInsert(file, uploadButtonId) {
             var formData = new FormData();
             formData.append('image', base64Image);
             formData.append('activity_id', uploadButtonId);
+            formData.append('image_reference', referenceNumber)
 
             insertImage(formData)
                 .then(() => resolve())
@@ -620,8 +720,8 @@ async function convertImageToFormDataAndInsert(file, uploadButtonId) {
               'src': URL.createObjectURL(files[i]),
               'alt': files[i].name,
               'class': 'img-fluid m-1', // Added margin for spacing
-              'width': '100',
-              'height': '100'
+              'width': '200',
+             
           });
 
           // Append the img element to the container
@@ -629,16 +729,55 @@ async function convertImageToFormDataAndInsert(file, uploadButtonId) {
       }
   });
 
+// Function to generate a random string with a specified length
+function generateRandomString(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        result += charset.charAt(randomIndex);
+    }
+    return result;
+}
+
+// Function to generate a random number within a specified range
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to generate a reference number of exactly 25 characters
+function generateReferenceNumber() {
+    const randomString = generateRandomString(13); 
+    const randomNumber = generateRandomNumber(100000, 999999);
+    return `REF-${randomString}-${randomNumber}`;
+}
+
+$(document).on("click", "#btnCloseModal", async function() {
+    
+
+      // Clear the selected image files
+      $('#imageUpload').val('');
+      // Clear the displayed images
+      $('#selectedImagesContainer').empty();
+      // Hide modal after processing the images
+      $('#uploadModal').modal('hide');
+  });
+
   $(document).on("click", "#btnUploadFile", async function() {
       // Get the selected image files
       var files = $('#imageUpload')[0].files;
       var uploadButtonId = $('#uploadModal').data('uploadButtonId');
+
+      const referenceNumber = generateReferenceNumber();
+      console.log("REFERENCE NUMBER")
+      console.log(referenceNumber);
+
       console.log('Clicked Upload button ID:', uploadButtonId);
 
       // Convert and upload each image sequentially
       for (var i = 0; i < files.length; i++) {
           try {
-              await convertImageToFormDataAndInsert(files[i], uploadButtonId);
+              await convertImageToFormDataAndInsert(files[i], uploadButtonId, referenceNumber);
               console.log(files[i].name);
           } catch (error) {
               console.error('Error processing file:', error);
@@ -675,40 +814,7 @@ async function convertImageToFormDataAndInsert(file, uploadButtonId) {
 
 // ----------------------------------------------------------------
 
-// Assuming you have an element with the ID 'activityImagesContainer' to display the images
-var activityImagesContainer = $('#activityImagesContainer');
 
-// Function to fetch and display images for a specific activity
-function fetchAndDisplayImages(activityId) {
-    $.ajax({
-        url: '/upload/retrieve_blob.php?activity_id=' + activityId,
-        type: 'GET',
-        success: function (data) {
-            console.log("BASE64 IMAGE");
-
-            // Check if data is null
-            if (data === null) {
-                console.error('Image data is null.');
-                return;
-            }
-
-            var imgElement = $('<img>').attr({
-                'src': 'data:image/jpeg;base64,' + data,
-                'class': 'img-fluid m-1',
-                'width': '100',
-                'height': '100'
-            });
-
-            // Append the img element to the container
-            activityImagesContainer.append(imgElement);
-        },
-        error: function (error) {
-            console.error('Error fetching images:', error);
-        }
-    });
-}
-
-fetchAndDisplayImages(1);
 // ----------------------------------------------------------------
 
             // FUNCTION CALLING
