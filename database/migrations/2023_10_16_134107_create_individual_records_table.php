84f5c5bc230ffa7cdf55238822c8ff398ae9a39b
RJ Marcial <richardjulius.marcial@benilde.edu.ph>
2024-03-31 14:30:36 +0800

    fixed duplicate column error on indiv  & history records table

--- database/migrations/2023_10_16_134107_create_individual_records_table.php
+++ database/migrations/2023_10_16_134107_create_individual_records_table.php
@@ -46,7 +46,6 @@ return new class extends Migration
             $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade');
             $table->softDeletes();
-            $table->enum('feeding_candidate', ['YES', 'NO'])->default('NO');
         });
 
         // DB::statement
